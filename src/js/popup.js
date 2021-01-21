3a94fbcc698b3c9a92c8a90cebcf359e1ff01df7
Ivan Klesov <ivklesov@gmail.com>
2021-01-21 20:56:40 +0300

    feat: complete rules page

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,10 +3,12 @@ import "./slider";
 const mainMenu = document.querySelector(".main-menu");
 // const newGame = document.querySelector(".new-game");
 const cards = document.querySelector(".cards");
-// const rules = document.querySelector(".rules");
+const rules = document.querySelector(".rules");
 // const settigns = document.querySelector(".settings");
 const allcards = document.querySelector(".all-cards");
+const rulesCategorie = document.querySelector(".rules-categorie");
 const backBtnCards = document.querySelector(".btn-back-cards");
+const backBtnRules = document.querySelector(".btn-back-rules");
 
 function openCategories(categorie) {
   mainMenu.classList.add("hidden");
@@ -22,6 +24,14 @@ cards.addEventListener("click", () => {
   openCategories(allcards);
 });
 
+rules.addEventListener("click", () => {
+  openCategories(rulesCategorie);
+});
+
 backBtnCards.addEventListener("click", () => {
   closeCategories(allcards);
+});
+
+backBtnRules.addEventListener("click", () => {
+  closeCategories(rulesCategorie);
 });
