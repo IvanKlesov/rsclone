a9e761648e6420a2212f1714e2acc22f543f8c27
Ivan Klesov <ivklesov@gmail.com>
2021-01-28 01:22:28 +0300

    feat: add listener for shop canvas elements

--- src/js/shop.js
+++ src/js/shop.js
@@ -1,19 +1,38 @@
 let currentColor;
+const blueCards = ["wheatField", "farm", "flowerGarden", "forest", "longboat", "mine", "appleOrchard", "trawler"];
+const greenCards = [
+  "bakery",
+  "store",
+  "flowerStore",
+  "cheeseDairy",
+  "furnitureFactory",
+  "vegetableMarket",
+  "groceryWarehouse",
+];
+const redCards = ["sushiBar", "cafe", "pizzeria", "diner", "restaurant"];
+const purpleCards = ["businessCenter", "stadium", "telecentre", "publishingHouse", "tax"];
+const attractionCards = ["port", "railwayStation", "shoppingCenter", "amusementPark", "radioTower", "airport"];
 
 export function createShop(color) {
   currentColor = color;
   const shopContent = document.querySelector(".shop-content");
+  if (document.querySelector(".canvas-shop")) {
+    shopContent.removeChild(document.querySelector(".canvas-shop"));
+  }
+  const canvasShop = document.createElement("canvas");
+  canvasShop.classList.add("canvas-shop");
+  shopContent.appendChild(canvasShop);
+  const ctx = canvasShop.getContext("2d");
   const padding = 20;
   const border = 7;
+  const elements = [];
   const widthShop = shopContent.offsetWidth - (border * 2 + padding * 2);
   const images = document.querySelectorAll(color);
-  const canvasShop = document.querySelector(".canvas-shop");
-  const ctx = canvasShop.getContext("2d");
   const arrayImages = Array.from(images);
   const widthImg = 180;
   const heightImg = 275;
-  const countImgBlock = Math.floor((widthShop - 100) / widthImg);
-  const margin = (widthShop - countImgBlock * widthImg + 100) / (countImgBlock * 2);
+  const countImgBlock = Math.floor((widthShop - 60) / widthImg);
+  const margin = (widthShop - countImgBlock * widthImg + 60) / (countImgBlock * 2);
 
   const countImgLine = Math.ceil(8 / countImgBlock);
   let heightShop;
@@ -31,6 +50,15 @@ export function createShop(color) {
     case 5:
       heightShop = heightImg * countImgLine + padding * 4 + padding * 3;
       break;
+    case 6:
+      heightShop = heightImg * countImgLine + padding * 5 + padding * 3;
+      break;
+    case 7:
+      heightShop = heightImg * countImgLine + padding * 6 + padding * 3;
+      break;
+    case 8:
+      heightShop = heightImg * countImgLine + padding * 7 + padding * 3;
+      break;
     default:
       heightShop = heightImg + padding * 3;
   }
@@ -46,10 +74,56 @@ export function createShop(color) {
     let x = margin;
     let y = 0;
     for (let i = 0; i < arrayImages.length; i += 1) {
+      switch (currentColor) {
+        case ".picture-blue":
+          elements.push({
+            name: blueCards[i],
+            left: x,
+            top: y,
+            width: 180,
+            height: 275,
+          });
+          break;
+        case ".picture-green":
+          elements.push({
+            name: greenCards[i],
+            left: x,
+            top: y,
+            width: 180,
+            height: 275,
+          });
+          break;
+        case ".picture-red":
+          elements.push({
+            name: redCards[i],
+            left: x,
+            top: y,
+            width: 180,
+            height: 275,
+          });
+          break;
+        case ".picture-purple":
+          elements.push({
+            name: purpleCards[i],
+            left: x,
+            top: y,
+            width: 180,
+            height: 275,
+          });
+          break;
+        case ".picture-attractions":
+          elements.push({
+            name: attractionCards[i],
+            left: x,
+            top: y,
+            width: 180,
+            height: 275,
+          });
+          break;
+      }
       ctx.drawImage(arrayImages[i], x, y, 180, 275);
       x += widthImg + margin;
       if (widthShop - x <= margin + widthImg) {
-        console.log(countImgLine);
         x = margin;
         y = y + heightImg + padding;
       }
@@ -57,6 +131,20 @@ export function createShop(color) {
   }
 
   drawShop();
+
+  canvasShop.addEventListener("click", (event) => {
+    const box = canvasShop.getBoundingClientRect();
+
+    const x = event.clientX - box.left;
+    const y = event.clientY - box.top;
+
+    elements.forEach((element) => {
+      if (y > element.top && y < element.top + element.height && x > element.left && x < element.left + element.width) {
+        alert(element.name);
+      }
+    });
+  });
+  console.log(elements);
 }
 
 console.log(currentColor);
