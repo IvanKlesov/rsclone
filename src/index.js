46aa1aed869c7ccc53e9d85ec08914993910e9b5
Ivan Klesov <ivklesov@gmail.com>
2021-01-28 21:53:36 +0300

    feat: "new game" started func get chat rooms

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ if (typeof module.hot !== "undefined") {
   module.hot.accept(); // eslint-disable-line no-undef
 }
 
+const newGame = document.querySelector(".new-game");
 const sendBtn = document.getElementById("send");
 const chatRoomsBtn = document.getElementById("chatRoomsBtn");
 const getOutRoomBtn = document.getElementById("getOutRoomBtn");
@@ -182,6 +183,11 @@ sendBtn.onclick = () => {
 };
 
 chatRoomsBtn.onclick = () => {
+  logMessage("chat rooms btn");
+  clientMessageMethods.getRooms(ws);
+};
+
+newGame.onclick = () => {
   logMessage("chat rooms btn");
   clientMessageMethods.getRooms(ws);
 };
